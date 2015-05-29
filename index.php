c4d6c545090e3bc9df17cd6901a9c4aae801ef64
MikaelCabral <don_cabral@hotmail.fr>
2015-05-29 16:38:09 +0200

    Maj

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	
 	$db = new PDO("mysql:dbname=dreamcommerce;host=10.32.195.220", 'idream', 'troiswa');
 	$db->exec("SET CHARACTER SET utf8");
-
+	require('./apps/function.php');
 	$page = 'home';
 	if (isset($_GET['page']) && !empty($_GET['page'])){
 		$page = $_GET['page'];
@@ -15,5 +15,10 @@
 		$id_produit = $_GET['id_produit'];
 
 	}
+	if (isset($_GET['id_user'])){
+		$id_user = $_GET['id_user'];
+
+	}
+
 	require('./views/skel.phtml');
 ?>
