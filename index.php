2e39ba8ceaec8fc8774c538857ffc10c13a76c59
MikaelCabral <don_cabral@hotmail.fr>
2015-05-29 14:30:01 +0200

    Maj

--- index.php
+++ index.php
@@ -8,5 +8,12 @@
 	if (isset($_GET['page']) && !empty($_GET['page'])){
 		$page = $_GET['page'];
 	}
+	if (isset($_GET['id_category'])){
+		$id_category = $_GET['id_category'];
+	}
+	if (isset($_GET['id_produit'])){
+		$id_produit = $_GET['id_produit'];
+
+	}
 	require('./views/skel.phtml');
 ?>
