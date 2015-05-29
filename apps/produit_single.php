ca3ef531a2443e4a63bd01c73a76a32a0d6148ce
MikaelCabral <don_cabral@hotmail.fr>
2015-05-29 15:16:27 +0200

    recup

--- apps/produit_single.php
+++ apps/produit_single.php
@@ -1,22 +1,31 @@
 <?php
-	if (isset($id_produit)) {
-	$tab = $db->query("SELECT * FROM produit WHERE id=".$id_produit)->fetch(PDO::FETCH_ASSOC);
-	if (isset($tab['nom']) && !empty($tab['nom']) &&
-		 isset($tab['date']) && !empty($tab['date']) && 
-		 isset($tab['prix']) && !empty($tab['prix']) &&
-		 isset($tab['description']) && !empty($tab['description']) && isset($tab['lien'])){
+if (isset($id_produit)) {
+	$id_produit=$db->quote($id_produit);
+	$tab = $db->query("SELECT produit.* , img.lien
+	FROM produit
+	JOIN img ON produit.id_produit = img.id_produit
+	WHERE id_produit= ".$id_produit )->fetch(PDO::FETCH_ASSOC);
+	if (isset($tab) && !empty($tab)) {
+		$lien = $tab['lien'];
+		$id_produit = $tab['id_produit'];
 		$nom = htmlentities($tab['nom']);
+		$reference = htmlentities($tab['reference']);
 		$date = $tab['date'];
-		$prix = $tab['prix'];
-		$lien = htmlentities($tab['lien']);
-		$description = balise($tab['description']);
+		$description = $tab['description'];
+		$duree = $tab['duree'];
+		$prix = $tab ['prix'];
 	}
 	else {
-		$commentaire = "Erreur lecture base de données";
-		require('./views/erreur.phtml');
-		die();
+	$erreur="Sujet non trouvé. ";
+	require('./views/erreur.phtml');
+	die();
 	}
-	}
+}
+else {
+	$erreur="Vous n'avez pas le droit d'accéder directement sans passer par la page d'accueil en haut à gauche, angle 180° à droite de là bas, en haut enfin tu vois ce que je veux dire non ?";
+	require('./views/erreur.phtml');
+}
+
 require('./views/produit_single.phtml');
 
 ?>
