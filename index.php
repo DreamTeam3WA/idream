f544dbd9ef9dc05f3f961f9e49bdd4ee78d731ae
Doppelganger217 <alex.ctx217@gmail.com>
2015-06-01 15:50:09 +0200

    alex

--- index.php
+++ index.php
@@ -17,6 +17,13 @@
 
 	$liste = array("category_list", "user_single","produit_single_commentaire_add", "produit_single");
 
+if (isset($_GET['ajax']))
+{
+	$page = $_GET['ajax'];
+	require('apps/'.$page.'.php');
+}
+else
+{
 
 	$page = 'home';
 	if (isset($_GET['page']) && !empty($_GET['page']) && in_array($_GET['page'], $liste)){
@@ -37,5 +44,5 @@
 		$id_user = $_GET['id_user'];
 	}
 
-	require('./views/skel.phtml');
+	require('./views/skel.phtml');}
 ?>
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -14,27 +14,24 @@ $('document').ready(function()
 	$('ps_commentaire').submit(function(info)
 	{
 		info.preventDefault();
-		$note = $(this).find('#note').val();
-		$commentaire_add = $(this).find('#commentaire_add').val();
-		$id_produit = $(this).find('#ps_id_produit').val();
-		$commentaire = $(this).find('#ps_comm').val();
-		$message = $(this).find('#contact-message').val();
+		note = $(this).find('#note').val();
+		commentaire_add = $(this).find('#commentaire_add').val();
+		id_produit = $(this).find('#ps_id_produit').val();
+		// $commentaire = $(this).find('#ps_comm').val();
+		commentaire = tinyMCE.get('ps_comm').getContent();
 		$.post($(this).attr('action'),
-			{"note":$note ,
-			 "action":$commentaire_add,
-			 "nom" : $nom,
-			 "id_produit" : $id_produit,
-			 "commentaires" :$commentaire
+			{"note":note ,
+			 "action":commentaire_add,
+			 "nom" : nom,
+			 "id_produit" : id_produit,
+			 "commentaires" : commentaire
 			}); 
 
 		$.get('index.php?ajax=produit_commentaire_liste',function(data)
 		{
 			$('#contact-content').html(data);
 		});
-		// function(data)
-		// {
-		// 	$('body').html(data);
-		// });
+		
 		return false;
 	});
 
