a51077674711e7b0941cb9d33754c4583bbc2bee
h-laurent <laurent.hansmaennel@gmail.com>
2015-06-01 10:52:46 +0200

    maj

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 	session_start();
 	
-	$db = new PDO("mysql:dbname=dreamcommerce;host=10.32.195.220", 'idream', 'troiswa');
+	$db = new PDO("mysql:dbname=dreamcommerce;host=10.32.195.200", 'idream', 'troiswa');
 	$db->exec("SET CHARACTER SET utf8");
 
 	$page = 'home';
 	if (isset($_GET['page']) && !empty($_GET['page'])){
 		$page = $_GET['page'];
+	}
+	if (isset($_GET['id_category'])){
+		$id_category = $_GET['id_category'];
 	}
 	require('./views/skel.phtml');
 ?>
