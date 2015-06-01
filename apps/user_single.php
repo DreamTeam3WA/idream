765c0d8563af6998ba48168943c82e17a1accd06
Doppelganger217 <alex.ctx217@gmail.com>
2015-06-01 13:24:09 +0200

    alex

--- apps/user_single.php
+++ apps/user_single.php
@@ -20,15 +20,15 @@
 
 		$i=0;
 		while (isset($tab[$i])){
-		$prenom_adresse = htmlentities($tab[$i]['prenom_adresse']);
-		$nom_adresse = htmlentities($tab[$i]['nom_adresse']);
-		$ligne1 = htmlentities($tab[$i]['ligne1']);
-		$ligne2 = htmlentities($tab[$i]['ligne2']);
-		$ville = htmlentities($tab[$i]['ville']);
-		$code = htmlentities($tab[$i]['code']);
-		$pays = htmlentities($tab[$i]['pays']);
-		$i++;
-		require('views/user_adresse.phtml');
+			$prenom_adresse = htmlentities($tab[$i]['prenom_adresse']);
+			$nom_adresse = htmlentities($tab[$i]['nom_adresse']);
+			$ligne1 = htmlentities($tab[$i]['ligne1']);
+			$ligne2 = htmlentities($tab[$i]['ligne2']);
+			$ville = htmlentities($tab[$i]['ville']);
+			$code = htmlentities($tab[$i]['code']);
+			$pays = htmlentities($tab[$i]['pays']);
+			$i++;
+			require('views/user_adresse.phtml');
 		}
 		require('views/user_single_end.phtml');
 	}
