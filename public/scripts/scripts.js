7bdcf8f2755faf55f1cbf2b48fd97c269a0a7cee
Pomzed <pomzed.pro@gmail.com>
2015-06-01 15:49:12 +0200

    eric : css form Connect user

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -7,7 +7,6 @@ $('document').ready(function()
 		$('.inscription').toggle(500);
 	})
 	$('.signin, .fermer_add_user').click(function(){
-		// aler	t("aaa");
 		$('.connection').toggle(500);
 	})
 })
