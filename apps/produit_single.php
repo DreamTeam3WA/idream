92213a2e42121001ef0e6c509fe73dc4505ff35b
MikaelCabral <don_cabral@hotmail.fr>
2015-06-05 20:50:13 +0200

    Add commentaire en fonction droits, correction de bugs, amélioration du site en général

--- apps/produit_single.php
+++ apps/produit_single.php
@@ -11,16 +11,24 @@ if (isset($_GET['id_produit']) && !empty($_GET['id_produit'])){
 	if (isset($tab) && !empty($tab)) {
 		$nom_produit = htmlentities($tab[0]['nom_produit']);
 		$reference = htmlentities($tab[0]['reference']);
-
 		$date = $tab[0]['date'];
 		$description = htmlentities($tab[0]['description']);
 		$duree = $tab[0]['duree'];
 		$prix = $tab[0]['prix'];
+		$note = $tab[0]['note'];
+		$commentaires = balise($tab[0]['commentaires']);
+		$date_avis = $tab[0]['date_avis'];
+		$prenom = htmlentities($tab[0]['prenom']);
 
 		require('./views/produit_single.phtml');
+		if (isset($_SESSION['id_user']) && !empty($_SESSION['id_user'])) {
+			require('./views/produit_add_commentaire.phtml');
+		}
 
 		$i=0;
-		while(isset($tab[$i])){
+		$note = $tab[$i]['note'];
+
+		while(isset($tab[$i]) && !empty($tab[$i]) && isset($tab[$i]['note']) && !empty($tab[$i]['note'])){
 			$note = $tab[$i]['note'];
 			$commentaires = balise($tab[$i]['commentaires']);
 			$date_avis = $tab[$i]['date_avis'];
