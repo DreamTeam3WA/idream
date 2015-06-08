aff2fd34360385807891e8e17673bd0cbce1be25
Doppelganger217 <alex.ctx217@gmail.com>
2015-06-08 23:16:27 +0200

    panier si connecté

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	// $_SESSION['id']=1; // a supprimer
 	
 
-	$db = new PDO("mysql:dbname=dreamcommerce;host=10.32.195.200", 'idream', 'troiswa');
-	/*$db = new PDO("mysql:dbname=dreamcommerce;host=127.0.0.1", 'root', 'troiswa');*/
+	//$db = new PDO("mysql:dbname=dreamcommerce;host=10.32.195.200", 'idream', 'troiswa');
+	$db = new PDO("mysql:dbname=dreamcommerce;host=127.0.0.1", 'root', '');
 
 	$db->exec("SET CHARACTER SET utf8");
 
